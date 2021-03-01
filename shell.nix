fce9f5c2a3ce2632db2bcfb14c5c2492398d9b69
Lionello Lunesu <lio+git@lunesu.com>
2021-02-28 19:41:37 -0800

    Add ipykernel and OPENBLAS_NUM_THREADS=1 to shell.nix

--- shell.nix
+++ shell.nix
@@ -2,10 +2,11 @@ with import <nixpkgs> { };
 let
   implicit = ps: ps.callPackage nix/implicit.nix {};
   nmslib = ps: ps.callPackage nix/nmslib.nix {};
-  python3' = python3.withPackages(ps: with ps; [ (implicit ps) h5py numpy annoy (nmslib ps) ]);
+  python3' = python3.withPackages(ps: with ps; [ (implicit ps) (nmslib ps) h5py numpy annoy ipykernel ]);
 in mkShell {
   buildInputs = [
     nodejs-12_x
     python3'
   ];
+  OPENBLAS_NUM_THREADS = 1; # Recommended by implicit
 }
