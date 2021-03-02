41c9abb030f1beec89ecfb6e0a1879cd3f318281
Lionello Lunesu <lio+git@lunesu.com>
2021-03-01 23:48:22 -0800

    Handle posted url

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express"
 import * as Path from "path"
 import helmet from "helmet"
+import { importFromURLCached } from "./parse"
+
+const SafeURL = "https://tidal.com/browse/"
 
 export const app = express()
 
@@ -10,9 +13,18 @@ function makeAbsolute(relative: string): string {
 
 app.use(helmet())
 
-app.post("/url", express.urlencoded({ extended: true }), (req, res) => {
-  console.log(req.body)
-  res.redirect("/")
+app.post("/url", express.urlencoded({ extended: true }), (req, res, next) => {
+  const playlist_url = req.body.playlist_url
+  if (typeof playlist_url !== "string" || playlist_url.startsWith(SafeURL)) {
+    res.status(400).send("Not a Tidal URL")
+  } else {
+    importFromURLCached(playlist_url)
+      .then((songs) => {
+        console.log(songs)
+        res.redirect("/")
+      })
+      .catch(next)
+  }
 })
 
 app.use("/js", express.static(makeAbsolute("../dist")))
