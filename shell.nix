31fef80c9acb13ebc632e65e729037d9d85a710d
Lionello Lunesu <lio+git@lunesu.com>
2022-09-16 11:38:06 -0700

    Add git to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,5 @@
-with import <nixpkgs> { };
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
 let
   # callPackage' = path: overrides: callPackage path (overrides // { stdenv = self.gcc8Stdenv; });
   # fixStdenv = drv: drv.overrideAttrs (_: { ca = gcc8Stdenv; });
@@ -7,25 +8,28 @@ let
   # python3gcc = python3.override { callPackage = callPackage'; };#Attrs (_: { stdenv = gcc8Stdenv; });
   #python3' = python3.withPackages(ps: with ps; [ (implicit ps) (nmslib ps) h5py numpy annoy ipykernel flask black ]);
   python37gcc = python37.override { stdenv = gccStdenv; };
-  python37' = python37.withPackages(ps: with ps; [ poetry ]);
-  python27' = python27.withPackages(ps: with ps; [ virtualenv numpy ]);
-in mkShell {
+  python37' = python37.withPackages (ps: with ps; [ poetry ]);
+  python27' = python27.withPackages (ps: with ps; [ virtualenv numpy ]);
+in
+mkShell {
   #name = "gcc-nix-shell";
   buildInputs = [
+    azure-cli
+    git
+    #lapack
+    #mkl
     nodejs-14_x
+    #openblas
+    #openmp
+    overmind
+    #poetry.python
     python37
+  ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Accelerate
     darwin.apple_sdk.frameworks.IOKit
-    #poetry.python
-    overmind
-    #lapack
-    #openblas
-    #mkl
-    #openmp
-    azure-cli
   ];
   OPENBLAS_NUM_THREADS = 1; # Recommended by implicit
-  PYTHONFAULTHANDLER = 1;   # Show SIGSEGV stack traces
+  PYTHONFAULTHANDLER = 1; # Show SIGSEGV stack traces
   NODE_ENV = "development";
   FLASK_ENV = "development";
 }
